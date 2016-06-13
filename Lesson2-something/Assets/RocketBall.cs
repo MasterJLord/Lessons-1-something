f710364815beef6ab1e53c5b77254f4c5b4d507c
MasterJLord <pouncerproductions@yahoo.com>
2016-06-12 22:05:42 -0700

    Second half

---
+++ Lesson2-something/Assets/RocketBall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketBall : MonoBehaviour {
+
+    public float kickAmount = 1.0f;
+
+    private Rigidbody m_myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        m_myRigidBody = transform.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_myRigidBody.AddForce(kickAmount, 0.0f, 0.0f, ForceMode.Impulse);
+        
+    }
+}
---
+++ Lesson2-something/Assets/WatchRocketBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WatchRocketBall : MonoBehaviour {
+
+    public GameObject theRocketBall;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(theRocketBall.transform);
+	}
+}
